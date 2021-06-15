30ae98396c6284cc5cd3eb0505ff21c9960c86ce
ej.jung <wjddmswn554@naver.com>
2021-06-15 19:28:44 +0900

    material

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
-interface props {
+interface props { 
 }
 
 interface checkBoxlist {
